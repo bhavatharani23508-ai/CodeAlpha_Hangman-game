8adeae10a7757cf29e30cce212cb5645551722f1
bhavatharani23508-ai <bhavatharani23508@gmail.com>
2026-05-12 16:53:38 +0530

    Add files via upload

---
+++ Stock Portfolio Tracker.py
@@ -0,0 +1,23 @@
+stocks = {
+    "GOOGL": 180,
+    "SMSG": 250,
+    "MSFT": 320,
+    "APPL": 140
+}
+total_amount = 0
+count = int(input("How many stocks do you want to enter? : "))
+for i in range(count):
+    name = input("\nEnter stock name : ").upper()
+    try:
+        quantity = int(input("Enter quantity : "))
+    except ValueError:
+        print("Please enter numbers only for quantity.")
+        continue
+    if name in stocks:
+        price = stocks[name]
+        investment = price * quantity
+        total_amount += investment
+        print("Investment Value :", investment)
+    else:
+        print("Stock not available.")
+print("\nTotal Investment :", total_amount)
